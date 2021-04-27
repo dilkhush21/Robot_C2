84908a3e0c2cab7790a4b276d275a103afdce4ef
dilkhush21 <dk8875402887@gmail.com>
2021-04-27 14:54:30 +0530

    a

--- robot_C2/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/robot_C2/src
-# Build directory: /home/pi/robot_C2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("driver")
--- robot_C2/build/driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/robot_C2/src/driver
-# Build directory: /home/pi/robot_C2/build/driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
